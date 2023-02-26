06e14517c29122697f2d2d15fac9dc1c9044b09d
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-26 18:36:16 +0200

    add

---
+++ src/softUni/SoftUni.java
@@ -0,0 +1,27 @@
+package softUni;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SoftUni {
+
+    private int capacity;
+    private List<Student> data;
+
+    public SoftUni(int capacity) {
+        this.capacity = capacity;
+        this.data = new ArrayList<>();
+    }
+
+    public int getCapacity() {
+        return this.capacity;
+    }
+
+    public int getCount() {
+        return data.size();
+    }
+
+    public void insert(Student student) {
+
+    }
+}
