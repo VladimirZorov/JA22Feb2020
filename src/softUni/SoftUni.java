c07965191c18b6b7171996dcc20bc3789349e415
Vladimir Zorov <vl.zorov@gmail.com>
2023-02-27 19:16:14 +0200

    add

--- src/softUni/SoftUni.java
+++ src/softUni/SoftUni.java
@@ -21,7 +21,45 @@ public class SoftUni {
         return data.size();
     }
 
-    public void insert(Student student) {
+    public String insert(Student student) {
+        Student student1;
+        if (data.size() >= capacity) {
+            return "The hall is full.";
+        }
+        for (Student student2 : data) {
+            if (student2.equals(student)) {
+                return "Student is already in the hall.";
+            }
+        }
+        data.add(student);
+        return String.format("Added student %s %s.", student.getFirstName(), student.getLastName());
+    }
 
+    public String remove(Student student) {
+        for (Student student1 : data) {
+            if (student1.equals(student)) {
+                return String.format("Removed student %s %s.", student1.getFirstName(), student1.getLastName());
+                data.remove(student);
+            }
+        }
+        return null;
+    }
+
+    public Student getStudent(String firstName, String lastName) {
+        for (Student student : data) {
+            if (student.getFirstName().equals(firstName) && student.getLastName().equals(lastName)) {
+                return student;
+            }
+        }
+        return null;
+    }
+
+    public String getStatistics() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("Hall size: %d", data.size())).append(System.lineSeparator());
+        for (Student student : data) {
+            sb.append(student.toString()).append(System.lineSeparator());
+        }
+        return sb.toString().trim();
     }
 }
